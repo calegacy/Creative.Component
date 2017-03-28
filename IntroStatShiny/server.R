1f7f17f8006a8cb84bd0b42e212e3a7a1f3463c3
calegacy <calegacy@iastate.edu>
2017-03-28 12:10:45 -0500

    Update server 3.28

--- IntroStatShiny/server.R
+++ IntroStatShiny/server.R
@@ -1,15 +1,7 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+
 
 #Libraries
 library(shiny)
-library(datasets)
 library(ggplot2)
 library(plotrix)
 library(MASS)
